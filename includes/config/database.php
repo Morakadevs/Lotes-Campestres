f98510a25b63e225e8c30d63194ce80bd442c2d4
[Morakadevs] <[wilton.moraca@gmail.com]>
2025-10-18 23:30:57 -0500

    Corrige conexión con base de datos DomCloud

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,10 +1,10 @@
 <?php 
 
 function conectarDB() : mysqli {
-    $host = 'localhost';
-    $user = 'lotescampestres';
-    $pass = 'doy62FHTQAd73_b)_8';
-    $db_name = 'lotescampestres';
+    $host = 'localhost'; 
+    $user = 'lotescampestres'; // el usuario MySQL de DomCloud (igual que tu usuario del sitio)
+    $pass = 'doy62FHTQAd73_b)_8'; // tu contraseña de MySQL (la misma que usas actualmente)
+    $db_name = 'lotescampestres_db'; // usa el nombre exacto de la base que se muestra en DomCloud
 
     $db = new mysqli($host, $user, $pass, $db_name);
 
