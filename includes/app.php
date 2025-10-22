910dbb2fa4327fd73bf84b79d831591100aed0cb
[Morakadevs] <[wilton.moraca@gmail.com]>
2025-10-22 14:27:44 -0500

    Corrige ruta de config/database.php para DomCloud

--- includes/app.php
+++ includes/app.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once 'config/database.php';
-require_once 'funciones.php';
+require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/funciones.php';
 
 use Model\ActiveRecord;
 
