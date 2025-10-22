fd9b4db01f3814c99ea60ec324619cb2ca04364a
[Morakadevs] <[wilton.moraca@gmail.com]>
2025-10-22 09:55:26 -0500

    Corrige orden de autoload en app.php

--- includes/app.php
+++ includes/app.php
@@ -1,14 +1,11 @@
 <?php
 
-require 'funciones.php';
-require 'config/database.php';
-require __DIR__ . '/../vendor/autoload.php';
-
-//Conectarnos a la base de datos
-$db = conectarDB();
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once 'config/database.php';
+require_once 'funciones.php';
 
 use Model\ActiveRecord;
-    
+
+// Conectarnos a la base de datos
+$db = conectarDB();
 ActiveRecord::setDB($db);
-
-
